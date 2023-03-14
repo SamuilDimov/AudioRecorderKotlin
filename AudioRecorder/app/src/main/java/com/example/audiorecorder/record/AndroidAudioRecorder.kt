942b88e8658a0009fa0fee69d3b1a56f96b94ef0
Dimov,Samuil S <samuildimov347@gmail.com>
2023-03-14 15:10:41 +0100

    AndroidAudioRecorder.kt
    added mic usage, format of recording, voice quality encoder, output file and Start and Stop functions

--- AudioRecorder/app/src/main/java/com/example/audiorecorder/record/AndroidAudioRecorder.kt
+++ AudioRecorder/app/src/main/java/com/example/audiorecorder/record/AndroidAudioRecorder.kt
@@ -1,5 +1,11 @@
 package com.example.audiorecorder.record
 import java.io.File
+import android.content.Context
+import android.media.MediaRecorder
+import android.os.Build
+import java.io.FileOutputStream
+import android.provider.MediaStore.Audio.Media
+
 
 class AndroidAudioRecorder(
     private val context: Context
@@ -8,13 +14,25 @@ class AndroidAudioRecorder(
     private var recorder: MediaRecorder? = null
     private fun createRecorder(): MediaRecorder{
         return if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.S){
-            
-        }
+            MediaRecorder(context)
+        } else MediaRecorder()
     }
     override fun start(outputFile: File){
+        createRecorder().apply{
+            setAudioSource(MediaRecorder.AudioSource.MIC)//microphone usage
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)//format of recording
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)//voice quality encoder
+            setOutputFile(FileOutputStream(outputFile).fd)//output file using file descriptor
 
+            prepare()
+            start()
+
+            recorder = this
+        }
     }
     override fun stop(){
-
+    recorder?.stop()
+        recorder?.reset()
+        recorder = null
     }
 }
